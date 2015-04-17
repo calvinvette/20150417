7dff60b4536b419b4e206928886225c38a8c6878
calvinvette <calvin@dragontale.com>
2015-04-17 12:08:42 -0400

    20150417@12.15

--- app/scripts/directives/customerView.js
+++ app/scripts/directives/customerView.js
@@ -1,7 +1,21 @@
 angular.module("MyApp").directive("customerView", function() {
 	console.log("customerView directive loaded...");
 	return {
-		
-		templateUrl: 'scripts/directives/CustomerView.html' 
+		restrict : 'ACE',
+		templateUrl: 'scripts/directives/CustomerView.html',
+		scope : {
+			'myVal1' : "@val1",
+			'myVal2' : "@val2",
+			'customer' : "=customer"
+		},
+		link : function(scope, elem, attrs) {
+//			var foo = "foo";
+//			setTimeout(function() { foo = "bar"; }, 1500);
+			scope.$watch(function() { return scope.customer.firstName}, 
+				function(val) {
+				console.log("changed");
+				elem.text("first: " + val);
+			});
+		}
 	};
 });
