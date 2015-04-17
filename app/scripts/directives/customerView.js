cb620555ebfd05e558587f2431b37d1cb3ccb4cf
calvinvette <calvin@dragontale.com>
2015-04-17 11:30:37 -0400

    20150417@11.20

--- app/scripts/directives/customerView.js
+++ app/scripts/directives/customerView.js
@@ -1,9 +1,7 @@
 angular.module("MyApp").directive("customerView", function() {
 	console.log("customerView directive loaded...");
 	return {
-		template: 'Customer: {{customer.firstName}} '
-			+ '{{customer.lastName}}' 
-			+ 'Phone: {{customer.phoneNumber}}'
-			+ 'Email: {{customer.email}}'
+		
+		templateUrl: 'scripts/directives/CustomerView.html' 
 	};
 });
